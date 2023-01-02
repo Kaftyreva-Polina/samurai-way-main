e5f204a7c7f598e262bcdcb4475df5bfd1175bcd
KaftyrevaPolina <kaft_ac@mail.ru>
2023-01-02 13:27:48 +0300

    Add components and props 1-4

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,41 @@
 import React from 'react';
 import './App.css';
+import Accordion from "./components/Accordion/Accordion";
+import {Rating} from "./components/Rating/Rating";
 
+
+// function declaration
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    console.log("App rendering")
+    //полезное что-то
+    //обязана вернуть JSX
+    return (
+        <div>
+            <PageTitle title={"This is APP component"}/>
+            <PageTitle title={"My friends"}/>
+            Article 1
+            <Rating value={3}/>
+            <Accordion titleValue={"Menu"}/>
+            <Accordion titleValue={"Users"}/>
+            Article 2
+            <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
+        </div>
+    );
 }
+
+type PageTitlePropsType = {
+    title: string
+}
+
+function PageTitle(props: PageTitlePropsType) {
+    console.log("PageTitle rendering")
+    return <h1>{props.title}</h1>
+}
+
 
 export default App;
